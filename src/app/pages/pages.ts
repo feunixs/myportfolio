9bac4ea87c0335296f2f67af956625a3e32b654f
feunix <feunix@outlook.co.id>
2023-01-15 15:20:04 +0700

    Add files via upload

---
+++ src/app/pages/pages.ts
@@ -0,0 +1,7 @@
+export const pages = [
+  { index: 0, name: "skills.md", route: "/skills" },
+  { index: 1, name: "experience.md", route: "/experience" },
+  { index: 2, name: "education.md", route: "/education" },
+  { index: 3, name: "projects.md", route: "/projects" },
+  { index: 4, name: "languages.md", route: "/languages" },
+];
